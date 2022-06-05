ef704091ad5a2533941f836058e9feaa5ca67508
Muh. Shofuwan Anwar <Hadoepan@gmail.com>
2022-06-05 15:18:26 +0700

    connecting wifi bug

--- src/self_wifi.h
+++ src/self_wifi.h
@@ -9,24 +9,26 @@ static uint32_t elapsed_time = 0;
 
 void wifi_init_system(){
     WiFi.begin((const char*)WIFI_SSID_STR, (const char*)WIFI_PWD_STR);
-    elapsed_time = millis();
 
     uint8_t flag_connected = 0;
 
     Serial.print("[WIFI] Connecting ");
+    elapsed_time = millis();
     while(WiFi.status() != WL_CONNECTED){
         if(GET_MS_TICK(1000)){
-            if(flag_connected >= 5){
+            if(flag_connected >= 8){
                 Serial.println("\n[WIFI] WiFi connect timeout!");
                 break;
             }
 
             Serial.print("*");
             flag_connected++;
+
+            elapsed_time = millis();
         }
     }
 
-    if(flag_connected >= 5){
+    if(flag_connected < 8){
         Serial.println("\n[WIFI] WiFi connected");
         Serial.println("[WIFI] IP Address : " + WiFi.localIP().toString());
     }else{
